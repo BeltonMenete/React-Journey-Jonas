6a88afcd5a3688890a0a1c8eeed4e1ae7c5e8fdb
codebelton <beltonmenete@hotmail.com>
2025-11-12 16:38:13 +0200

    add styles

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -90,15 +90,6 @@ function Footer() {
    const openHour = 8;
    const closeHour = 22;
    const isOpen = hour >= openHour && hour <= closeHour;
-   console.log(isOpen);
-
-   /* if (!isOpen) {
-      return (
-         <p>
-            We're happy to welcome you between {openHour}:00 and {closeHour}:00
-         </p>
-      );
-   } */
 
    return <footer className='footer'>{isOpen && <Order closeHour={closeHour} />}</footer>;
 }
