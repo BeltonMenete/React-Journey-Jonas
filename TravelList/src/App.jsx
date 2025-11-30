8fa3d8471898001fd4370da5f0ba9b4f2bef8286
codebelton <beltonmenete@hotmail.com>
2025-11-30 21:08:26 +0200

    fix all problems

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -1,6 +1,11 @@
+import './App.css';
 import { useState } from 'react';
 
-import './App.css';
+const initialItems = [
+   { id: 1, description: 'Passports', quantity: 2, packed: false },
+   { id: 2, description: 'Socks', quantity: 12, packed: false },
+   { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
 
 export default function App() {
    return (
@@ -24,9 +29,26 @@ function Form() {
    );
 }
 function PackingList() {
-   return <div className='list'>List</div>;
+   return (
+      <div className='list'>
+         <ul>
+            {initialItems.map((item) => (
+               <Item item={item} key={item.id} />
+            ))}
+         </ul>
+      </div>
+   );
 }
-
+function Item({ item }) {
+   return (
+      <li id={item.id}>
+         <span>
+            {item.quantity} {item.description}
+         </span>
+         <button>❌</button>
+      </li>
+   );
+}
 function Stats() {
    return (
       <footer className='stats'>
