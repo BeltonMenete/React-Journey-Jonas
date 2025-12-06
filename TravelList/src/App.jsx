382ad23d62e769684306cac4b4dc9f135f86d515
codebelton <beltonmenete@hotmail.com>
2025-12-06 22:35:11 +0200

    Try add render packing component inside form comp

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css';
 import { useState } from 'react';
 
-const initialItems = [
-   { id: 1, description: 'Passports', quantity: 2, packed: false },
-   { id: 2, description: 'Socks', quantity: 12, packed: false },
-   { id: 3, description: 'Charger', quantity: 1, packed: true },
-];
-
 export default function App() {
    return (
       <>
          <Logo />
          <Form />
-         <PackingList />
+
          <Stats />
       </>
    );
@@ -22,18 +16,52 @@ function Logo() {
 }
 
 function Form() {
+   const [Items, setItems] = useState([
+      { id: 1, description: 'Passports', quantity: 2, packed: false },
+      { id: 2, description: 'Socks', quantity: 12, packed: false },
+      { id: 3, description: 'Charger', quantity: 1, packed: true },
+   ]);
+   const [description, setDescription] = useState('');
+   const [quantity, setQuantity] = useState(1);
+   const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if (!description.trim()) return;
+
+      const newItem = { description, quantity, packed: false, id: Date.now() };
+
+      setItems(Items.push(newItem));
+
+      // reset form default this.state.
+      setDescription('');
+      setQuantity(1);
+   };
    return (
-      <form className='add-form'>
-         <h3>What do you need for your 😍 Trip?</h3>
-         <select name='' id=''>
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-         </select>
-      </form>
+      <>
+         <form className='add-form' onSubmit={handleSubmit}>
+            <h3>What do you need for your 😍 Trip?</h3>
+            <select name='' id='' onChange={(e) => setQuantity(+e.target.value)} value={quantity}>
+               {Array.from({ length: 20 }, (_, i) => i + 1).map((c) => (
+                  <option value={c} key={c}>
+                     {c}
+                  </option>
+               ))}
+            </select>
+            <input
+               type='text'
+               name=''
+               id=''
+               placeholder='item...'
+               value={description}
+               onChange={(e) => setDescription(e.target.value)}
+            />
+            <button>Add</button>
+         </form>
+         <PackingList />
+      </>
    );
 }
-function PackingList() {
+function PackingList({ initialItems }) {
    return (
       <div className='list'>
          <ul>
